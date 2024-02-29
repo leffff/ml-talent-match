32dea67bb28a11212104e629e55e690c1e84c63d
leffff <levnovitskiy@gmail.com>
2024-02-29 18:09:35 +0300

    .md added to parser

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import textract
+from bs4 import BeautifulSoup
+import markdown
 
 
 class ResumeParser:
@@ -17,9 +19,17 @@ class ResumeParser:
         text = textract.process(filename).decode("utf-8")
         return text
 
-    def read_image(self, filename): 
+    def read_image(self, filename) -> str: 
         text = textract.process(filename).decode("utf-8") # можно доабавить аргумент language, но надо качать тесеракт textract.process(filename, language="ru").decode("utf-8")
         return text
+
+    def read_md(self, filename) -> str: 
+        f = open(filename, 'r')
+        html = markdown.markdown( f.read() )
+
+        text = ''.join(BeautifulSoup(html).findAll(text=True))
+        return text
+
 
     def parse(self, filename: str) -> str:
         if filename.endswith('.pdf'):
@@ -30,6 +40,9 @@ class ResumeParser:
 
         elif filename.endswith('.doc'):
             return self.read_doc(filename)
+
+        elif filename.endswith('.md'):
+            return self.read_md(filename)
 
         elif filename.endswith('.txt'):
             return Error()
