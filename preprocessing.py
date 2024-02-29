855337431b0ef01fb491322081954a0278b91c9d
leffff <levnovitskiy@gmail.com>
2024-02-29 19:31:34 +0300

    naming change

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import re
 
 
-def remove_keys(input_string: str) -> str:
+def preprocess(input_string: str) -> str:
     pattern = r'\[(\w+)\s*:\s*([^\]]+)\]'
     cleaned_string = re.sub(pattern, r'\2', input_string)
     return cleaned_string
